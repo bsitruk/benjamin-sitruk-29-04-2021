15f2f4c81585e97988b0c4483eb96da529ef20f8
bsitruk <no-mail@google.com>
2021-05-03 11:00:44 +0300

    Display UnitSwitcher as IconButton

--- src/components/UnitSwitcher.tsx
+++ src/components/UnitSwitcher.tsx
@@ -1,5 +1,5 @@
+import { IconButton, Text } from "@chakra-ui/react";
 import * as React from "react";
-import { Button } from "@chakra-ui/react";
 import { useUnit, useUnitValue } from "../features/unit/hooks";
 import { Unit } from "../features/unit/types";
 
@@ -8,18 +8,17 @@ export const UnitSwitcher = () => {
   const nextUnit = useUnitValue(Unit.CELCIUS, Unit.FAHRENHEIT);
 
   return (
-    <Button
+    <IconButton
       size="md"
       fontSize="xl"
       fontFamily="fira"
       fontWeight="bold"
       variant="outline"
       color="current"
+      icon={<Text>{nextUnit}</Text>}
       onClick={toggleUnit}
       aria-label={`Switch to ${nextUnit} unit`}
-    >
-      {nextUnit}
-    </Button>
+    />
   );
 };
 
